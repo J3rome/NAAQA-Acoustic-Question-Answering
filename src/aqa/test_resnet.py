98322f4fe6507b039bd2231b2b1534d232603ca7
J3romee <jerome.abdelnour@usherbrooke.ca>
2019-06-23 16:15:42 -0400

    - removed commented code

--- src/aqa/test_resnet.py
+++ src/aqa/test_resnet.py
@@ -421,9 +421,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-        # TODO : Restore pretrained FiLM network
         # TODO : Extract Beta And Gamma Parameters + T-SNE
         # TODO : Feed RAW image directly to the FiLM network
         # TODO : Options for preprocessing (Feature Exctraction) to minimize training time ? ( Quantify the increase in training time)
@@ -434,58 +431,6 @@ if __name__ == "__main__":
         #        When feeding directly to FiLM, we can use original size ?
         # TODO : What is the optimal size for our spectrograms ?
         # TODO : Train with different amount of residual blocks. Other modifications to the architecture ?
-    #
-    #     for one_set in set_type:
-    #
-    #         print("Load dataset -> set: {}".format(one_set))
-    #         dataset_args["which_set"] = one_set
-    #         dataset = dataset_cstor(**dataset_args)
-    #
-    #         # hack dataset to only keep one game by image
-    #         image_id_set = {}
-    #         games = []
-    #         for game in dataset.games:
-    #             if game.image.id not in image_id_set:
-    #                 games.append(game)
-    #                 image_id_set[game.image.id] = 1
-    #
-    #         dataset.games = games
-    #         no_images = len(games)
-    #
-    #         source_name = os.path.basename(img_input.name[:-2])
-    #         dummy_tokenizer = DummyTokenizer()
-    #         batchifier = batchifier_cstor(tokenizer=dummy_tokenizer, sources=[source_name])
-    #         iterator = Iterator(dataset,
-    #                             batch_size=batch_size,
-    #                             pool=cpu_pool,
-    #                             batchifier=batchifier)
-    #
-    #         ############################
-    #         #  CREATE FEATURES
-    #         ############################
-    #         print("Start computing image features...")
-    #         filepath = os.path.join(out_dir, "{}_features.h5".format(one_set))
-    #         with h5py.File(filepath, 'w') as f:
-    #
-    #             ft_shape = [int(dim) for dim in ft_output.get_shape()[1:]]
-    #             ft_dataset = f.create_dataset('features', shape=[no_images] + ft_shape, dtype=np.float32)
-    #             idx2img = f.create_dataset('idx2img', shape=[no_images], dtype=np.int32)
-    #             pt_hd5 = 0
-    #
-    #             for batch in tqdm(iterator):
-    #                 feat = sess.run(ft_output, feed_dict={img_input: numpy.array(batch[source_name])})
-    #
-    #                 # Store dataset
-    #                 batch_size = len(batch["raw"])
-    #                 ft_dataset[pt_hd5: pt_hd5 + batch_size] = feat
-    #
-    #                 # Store idx to image.id
-    #                 for i, game in enumerate(batch["raw"]):
-    #                     idx2img[pt_hd5 + i] = game.image.id
-    #
-    #                 # update hd5 pointer
-    #                 pt_hd5 += batch_size
-    #             print("Start dumping file: {}".format(filepath))
-    #         print("Finished dumping file: {}".format(filepath))
+
 
 
