5427820ad4dfe011110054f7e85db2bd1f905d29
J3romee <jerome.abdelnour@usherbrooke.ca>
2019-07-31 21:07:37 -0400

    * Moved transforms out of dataset

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ from preprocessing import create_dict_from_questions, extract_features
 
 # NEW IMPORTS
 from models.torch_film_model import CLEAR_FiLM_model
-from data_interfaces.torch_dataset import CLEAR_dataset, CLEAR_collate_fct, ToTensor, ResizeImg, ImgBetweenZeroOne  # FIXME : ToTensor should be imported from somewhere else. Utils ?
+from data_interfaces.torch_dataset import CLEAR_dataset, CLEAR_collate_fct
+from data_interfaces.transforms import ToTensor, ResizeImg, ImgBetweenZeroOne
 from models.torchsummary import summary     # Custom version of torchsummary to fix bugs with input
 import torch
 import time
