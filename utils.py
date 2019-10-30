d38af3f0b7619269ace6f445c26117785a626b39
J3romee <jerome.abdelnour@usherbrooke.ca>
2019-10-30 03:27:00 -0400

    * Option to beautify attributes

--- utils.py
+++ utils.py
@@ -346,7 +346,7 @@ def save_gamma_beta_h5(gammas_betas, set_type, folder, filename=None, nb_vals=No
         return nb_val_to_write
 
 
-def get_answer_to_family_map(attributes_filepath, to_lowercase=True):
+def get_answer_to_family_map(attributes_filepath, to_lowercase=True, reduced_text=False):
     attributes = read_json(attributes_filepath)
 
     answer_to_family = {"<unk>": "unknown"}  # FIXME : Quantify what is the impact of having an unknown answer
@@ -354,8 +354,12 @@ def get_answer_to_family_map(attributes_filepath, to_lowercase=True):
     for family, answers in attributes.items():
         for answer in answers:
             the_answer = answer
+            if reduced_text and 'of the scene' in the_answer:
+                the_answer = str(answer.split(' ')[0][:3])
+
             if to_lowercase:
-                the_answer = answer.lower()
+                the_answer = the_answer.lower()
+
             # If there is duplicated answers, they will be assigned to the first occurring family
             if the_answer not in answer_to_family:
                 answer_to_family[the_answer] = family
