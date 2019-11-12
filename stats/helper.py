3f5219659ac20363acb7390172b5109823897a15
J3romee <jerome.abdelnour@usherbrooke.ca>
2019-11-11 22:45:29 -0500

    + "Confusion matrix" scene per position
    * Plot 2d matrix helper

--- stats/helper.py
+++ stats/helper.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
 
-from utils import read_json
+from utils.file import read_json
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
 
-from utils import get_answer_to_family_map
+from utils.generic import get_answer_to_family_map
 
 # TODO : Retrieve scenes ? Questions ?
 # TODO : If adding more attributes (Like presence of relation), should add them in the processing instead of here. We should mainly read and arrange stuff arond, not post process
@@ -28,7 +30,7 @@ def autolabel_bar(ax, rects):
 
 
 # Plotting
-def show_discrete_hist(data, key=None, title=None, label=None, sort_key_fct=None, show_fig=False,
+def show_discrete_hist(data, key=None, title=None, legend_label=None, sort_key_fct=None, show_fig=False,
                                 export_filepath=None, fig_ax=None, position='center', capitalize=True, bar_width=0.4,
                                 norm_hist=False, all_x_labels=None):
     show_fig = show_fig and export_filepath is None
@@ -72,7 +74,7 @@ def show_discrete_hist(data, key=None, title=None, label=None, sort_key_fct=None
     elif position == 'right':
         x = x + (bar_width/2)
 
-    rects = ax.bar(x, counts, width=bar_width, align='center', label=label)
+    rects = ax.bar(x, counts, width=bar_width, align='center', label=legend_label)
     ax.set_xticks(original_x)
     ax.set_xticklabels(labels, rotation=90)
 
@@ -190,7 +192,7 @@ def sort_correct_incorrect_predictions(predictions):
     }
 
 
-def plot_distribution_per_question_family(train_predictions, val_predictions, all_x_labels=None, norm_hist=False,
+def plot_predictions_distribution_per_question_family(train_predictions, val_predictions, all_x_labels=None, norm_hist=False,
                                           show_fig=False):
     keys_titles = [('correct', 'Correct Answer'),
                    ('correct_family', 'Incorrect Answer -- Correct Family'),
@@ -202,9 +204,9 @@ def plot_distribution_per_question_family(train_predictions, val_predictions, al
         fig, ax = plt.subplots(1, 1)
         ax.set_title(title)
         show_discrete_hist(train_predictions[key], key="ground_truth_answer_family", all_x_labels=all_x_labels,
-                           fig_ax=(fig, ax), position='left', label="Train", norm_hist=norm_hist)
+                           fig_ax=(fig, ax), position='left', legend_label="Train", norm_hist=norm_hist)
         show_discrete_hist(val_predictions[key], key="ground_truth_answer_family", all_x_labels=all_x_labels,
-                           fig_ax=(fig, ax), position='right', label='Val', norm_hist=norm_hist)
+                           fig_ax=(fig, ax), position='right', legend_label='Val', norm_hist=norm_hist)
         ax.legend()
 
         fig.tight_layout()
@@ -258,21 +260,6 @@ def plot_predictions_confidence(train_predictions, val_predictions, question_fam
         plt.show()
 
 
-def separate_preds_ground_truth_old(processed_predictions, filter_fct=None):
-
-    if filter_fct:
-        processed_predictions = filter(filter_fct, processed_predictions)
-
-    predictions = []
-    ground_truths = []
-
-    for processed_prediction in processed_predictions:
-        predictions.append(processed_prediction['prediction'])
-        ground_truths.append(processed_prediction['ground_truth'])
-
-    return predictions, ground_truths
-
-
 def separate_preds_ground_truth(processed_predictions, attribute=None):
 
     predictions = defaultdict(list)
@@ -294,9 +281,126 @@ def separate_preds_ground_truth(processed_predictions, attribute=None):
     return predictions, ground_truths
 
 
+def plot_2d_matrix(matrix, xlabels, ylabels=None, title=None, xaxis_name='predictions', yaxis_name="ground truth",
+                   show_fig=True, colormap=plt.cm.Blues, add_annotations=True, normalize=False):
 
-from sklearn.metrics import confusion_matrix
-from sklearn.utils.multiclass import unique_labels
+    if ylabels is None:
+        ylabels = xlabels
+
+    if normalize:
+        if type(normalize) == int:
+            norm_ax = normalize
+        else:
+            norm_ax = 1
+
+        sums = matrix.sum(axis=norm_ax)[:, np.newaxis]
+        sums[sums == 0] = 1
+        matrix = matrix / sums
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(matrix, interpolation='nearest', cmap=colormap)
+    # FIXME : Colorbar is too big
+    ax.figure.colorbar(im, ax=ax)#, boundaries=[0, max_val])
+    ax.set(xticks=np.arange(matrix.shape[1]), yticks=np.arange(matrix.shape[0]), yticklabels=ylabels,
+           title=title, ylabel=yaxis_name.capitalize(), xlabel=xaxis_name.capitalize())
+    ax.set_xticklabels(xlabels, rotation=90)
+    ax.axis('image')
+
+    if add_annotations:
+        # Loop over data dimensions and create text annotations.
+        fmt = '.2f' if normalize else 'd'
+
+        min_val = matrix.min()
+        thresh = min_val + (matrix.max() - min_val)/2
+
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                value = matrix[i, j]
+                if value > 0:
+                    string = format(value, fmt)
+                    if normalize and (value < 0.01 or value > 0.99):
+                        string = "~" + string
+
+                    ax.text(j, i, string,
+                            ha="center", va="center", fontsize=6,
+                            color="white" if value > thresh else "black")
+
+    fig.tight_layout()
+
+    if show_fig:
+        plt.show()
+
+    return fig, ax
+
+
+def scene_object_per_position(scenes, attribute='instrument', max_scene_length=None, attribute_processing=None):
+    if max_scene_length is None:
+        # Assume fixed scenes. We could also rerieve the max scene length
+        max_scene_length = len(scenes[0]['definition']['objects'])
+
+    positions = defaultdict(lambda: [0] * max_scene_length)
+    for scene in scenes:
+        for i, obj in enumerate(scene['definition']['objects']):
+            attribute_value = obj[attribute]
+            if callable(attribute_processing):
+                attribute_value = attribute_processing(attribute_value)
+            positions[attribute_value][i] += 1
+
+    return positions
+
+
+def plot_attribute_per_position_matrix(attribute_per_position, attribute_name, title=None, show_fig=False,
+                                       colormap=plt.cm.Blues, add_annotations=True, attribute_processing=None):
+    """
+        Column : Position
+        Row : Attributes
+    """
+    # FIXME : Not sure the order will be the same that with items()
+    attribute_values = list(attribute_per_position.keys())
+
+    if callable(attribute_processing):
+        attribute_values = [attribute_processing(attr) for attr in attribute_values]
+
+    attribute_values = [str(val).capitalize() if val is not None else "None" for val in attribute_values]
+
+    nb_attr = len(attribute_values)
+    nb_pos = len(list(attribute_per_position.values())[0])
+    matrix = np.zeros((nb_attr, nb_pos), dtype=np.int32)
+    attr_idx = 0
+    for attr, positions in attribute_per_position.items():
+        matrix[attr_idx, :] = positions
+        attr_idx += 1
+
+    if title is None:
+        title = f"[{attribute_name.capitalize()}]Scene objects per position"
+
+    fig_ax = plot_2d_matrix(matrix, range(nb_pos), attribute_values, title=title, show_fig=show_fig, colormap=colormap,
+                            xaxis_name="Position", yaxis_name=attribute_name.capitalize(),
+                            add_annotations=add_annotations)
+
+    return fig_ax
+
+
+def plot_scene_distribution_per_attribute(scenes, attribute_name, title=None, legend_label=None,
+                                          norm_hist=False, fig_ax=None, show_fig=False, attribute_processing=None):
+    attribute_list = []
+    for scene in scenes:
+        for i, obj in enumerate(scene['definition']['objects']):
+            value = obj[attribute_name]
+
+            if callable(attribute_processing):
+                value = attribute_processing(value)
+
+            value = str(value) if value is not None else "None"
+            attribute_list.append(value)
+
+    if title is None:
+        title = f"[{attribute_name.capitalize()}]Scene distribution"
+
+    fig_ax = show_discrete_hist(attribute_list, title=title,
+                                fig_ax=fig_ax, legend_label=legend_label, norm_hist=norm_hist, show_fig=show_fig)
+
+    return fig_ax
 
 
 def plot_confusion_matrix(predictions, ground_truth, normalize=False, title=None, show_fig=True,
@@ -306,39 +410,11 @@ def plot_confusion_matrix(predictions, ground_truth, normalize=False, title=None
     conf_matrix = confusion_matrix(ground_truth, predictions)
     labels = unique_labels(predictions, ground_truth)
 
-    if normalize:
-        conf_matrix = conf_matrix / conf_matrix.sum(axis=1)[:, np.newaxis]
-
     if title is None:
         title = "Confusion Matrix"
 
-    fig, ax = plt.subplots()
-    im = ax.imshow(conf_matrix, interpolation='nearest', cmap=colormap)
-    ax.figure.colorbar(im, ax=ax)
-    ax.set(xticks=np.arange(conf_matrix.shape[1]), yticks=np.arange(conf_matrix.shape[0]), yticklabels=labels,
-           title=title, ylabel="Ground Truth", xlabel="Predictions")
-    ax.set_xticklabels(labels, rotation=90)
-    ax.axis('image')
-
-    if add_annotations:
-        # Loop over data dimensions and create text annotations.
-        fmt = '.2f' if normalize else 'd'
-        always_black = (conf_matrix.max() - conf_matrix.min()) < 50
-        thresh = conf_matrix.max() / 2
-
-        for i in range(conf_matrix.shape[0]):
-            for j in range(conf_matrix.shape[1]):
-                if conf_matrix[i, j] > 0:
-                    ax.text(j, i, format(conf_matrix[i, j], fmt),
-                            ha="center", va="center",
-                            color="white" if conf_matrix[i, j] > thresh and not always_black else "black")
-
-    fig.tight_layout()
-
-    if show_fig:
-        plt.show()
-
-    return fig, ax
+    return plot_2d_matrix(conf_matrix, labels, title=title, normalize=normalize, show_fig=show_fig, colormap=colormap,
+                          xaxis_name='predictions', yaxis_name="ground truth", add_annotations=add_annotations)
 
 
 if __name__ == "__main__":
