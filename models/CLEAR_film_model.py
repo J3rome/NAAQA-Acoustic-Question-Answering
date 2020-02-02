af8ac3dfd2035e7b949e22a6a1f81b120885336e
j3rome <jerome.abdelnour@usherbrooke.ca>
2020-02-02 13:45:24 -0500

    * Moved feature extractor in it's own file

--- models/CLEAR_film_model.py
+++ models/CLEAR_film_model.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
+from models.feature_extractor import Resnet_feature_extractor
 from utils.random import set_random_state, get_random_state
 
 # Replicate tensorflow 'SAME' padding (Taken from https://github.com/mlperf/inference/blob/master/others/edge/object_detection/ssd_mobilenet/pytorch/utils.py#L40)
@@ -402,79 +403,6 @@ class CLEAR_FiLM_model(nn.Module):
         if self.current_device != device:
             self.current_device = device
             super(CLEAR_FiLM_model, self).to(device, dtype, non_blocking)
-
-class Resnet_feature_extractor(nn.Module):
-    def __init__(self, resnet_version=101, layer_index=6, no_grads=True):      # TODO : Add Parameter to unfreeze some layers
-        super(Resnet_feature_extractor, self).__init__()
-        # FIXME : Try with other ResNet
-
-        # Resnet 101 layers
-        # 0 -> Conv2d
-        # 1 -> BatchNorm2d
-        # 2 -> ReLU
-        # 3 -> MaxPool2d
-        # 4 -> First Block
-            # 0 -> First Bottleneck
-                # conv1
-                # bn1
-                # conv2
-                # bn2
-                # conv 3
-                # bn3
-                # relu
-                # downsample (Only in first block)
-                    # 0 -> Conv2d
-                    # 1 -> BatchNorm2d
-            # 1 -> Second Bottleneck
-            # 2 -> Third Bottleneck
-
-        # 5 -> Second Block
-            # 0 to 2 -> Bottlenecks
-
-        # 6 -> Third Block
-            # 0 to 22 -> Bottlenecks
-
-        # 7 -> Fourth Block
-            # 0 to 2 -> Bottlenecks
-
-        # 8 -> AdaptiveAvgPool2d
-        # 9 -> Linear
-
-        assert resnet_version == 101, 'Only Resnet-101 is implemented.'
-
-        resnet = torchvision.models.resnet101(pretrained=True)
-
-        self.extractor = nn.Sequential(*list(resnet.children())[:layer_index+1])
-
-        if no_grads:
-            for param in self.extractor.parameters():
-                param.requires_grad = False
-
-    def forward(self, image):
-        return self.extractor(image)
-
-    def get_last_bottleneck(self):
-        last_elem = self.extractor[-1]
-
-        # The chosen layer must be a bottleneck layer, otherwise this will result in an infinite loop
-        while not isinstance(last_elem, torchvision.models.resnet.Bottleneck):
-            last_elem = last_elem[-1]
-
-        return last_elem
-
-    def get_out_channels(self):
-        last_bottleneck = self.get_last_bottleneck()
-
-        return last_bottleneck.bn3.num_features
-
-    def get_output_shape(self, input_image, channel_first=True):
-        output = self(input_image)
-        output_size = output.size()[1:]     # Remove batch size dimension
-
-        if channel_first:
-            return output_size
-
-        return [output_size[1], output_size[2], output_size[0]]
 
 
 if __name__ == "__main__":
